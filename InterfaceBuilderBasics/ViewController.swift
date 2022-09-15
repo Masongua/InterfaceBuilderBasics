c6c069a3088a365150ed3c54e1ce2d416cb1d7a2
Mason Guaman <masonstalin@gmail.com>
2022-09-15 14:05:26 -0400

    th

--- InterfaceBuilderBasics/ViewController.swift
+++ InterfaceBuilderBasics/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+    @IBOutlet weak var mainLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+    
     }
 
-
+    @IBAction func changeTitle(_ sender: Any) {
+        
+        print("This app sucks")
+    }
+    
 }
 
